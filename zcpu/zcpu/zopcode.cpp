7e54e5d6d8d1956d420c000f16d85493ff58b9ed
zsteve <steve@0x4c.phatcode.net>
2013-12-09 09:54:59 +1100

    added more code to ZCpu

--- zcpu/zcpu/zopcode.cpp
+++ zcpu/zcpu/zopcode.cpp
@@ -1,4 +1,23 @@
+#include "zopcode.h"
 #include "../../zglobal/zglobal.h"
 #include "../../zglobal/zglobaldefines.h"
 #include "../../zmemory/zmemory/zmemory.h"
 
+using namespace ZCpuOps;
+
+ZOpcode::ZOpcode() : operandTypes(0){
+    this->operandCount=ZOPCOUNT_0;
+    this->opcodeName=UNDEFINED;
+    this->opcodeType=ZOPTYPE_LONG;
+};
+
+ZOpcode::ZOpcode(ulong addr, ZMemory& zMem) : operandTypes(0){
+    decodeOp(addr, zMem);
+};
+
+/**
+ * decodes opcode
+ */
+ZOpcode::decodeOp(ulong addr, ZMemory& zMem){
+    // first, we must look up the opcode number
+};
